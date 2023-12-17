e8716dac32a7790e3a6477a75975391a070ae121
Filippo Costa <filippo@neysofu.me>
2023-12-17 15:13:20 +0100

    macOS support

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,11 @@ main(void)
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	// glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	/* Enables macOS compatibility. See here:
+	 * <https://www.glfw.org/docs/latest/compat.html#compat_osx>
+	 */
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
 	GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
 	if (window == NULL) {
